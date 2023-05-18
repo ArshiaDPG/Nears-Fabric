3a67ce1856efad6ab9361da3bd2696004fbf8083
DP <digitalpeargaming@gmail.com>
2023-05-17 18:00:45 -0700

    supported blocks for all plants are now determined by tags

--- src/main/java/net/digitalpear/nears/common/blocks/SoulBerryBushBlock.java
+++ src/main/java/net/digitalpear/nears/common/blocks/SoulBerryBushBlock.java
@@ -5,7 +5,6 @@ import net.digitalpear.nears.init.tags.NBlockTags;
 import net.minecraft.block.*;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
