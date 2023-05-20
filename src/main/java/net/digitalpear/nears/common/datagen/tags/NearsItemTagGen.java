a9fa5c1476aff47096314de4badf96b3ef91e891
DP <digitalpeargaming@gmail.com>
2023-05-19 17:23:20 -0700

    nears are now piglin food

--- src/main/java/net/digitalpear/nears/common/datagen/tags/NearsItemTagGen.java
+++ src/main/java/net/digitalpear/nears/common/datagen/tags/NearsItemTagGen.java
@@ -7,6 +7,7 @@ import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.item.Item;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.ItemTags;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -26,6 +27,8 @@ public class NearsItemTagGen  extends FabricTagProvider<Item> {
     @Override
     protected void configure(RegistryWrapper.WrapperLookup arg) {
         getOrCreateTagBuilder(NItemTags.NETHER_FRUITS).add(NItems.NEAR, NItems.FAAR, NItems.SOUL_BERRIES);
+
+        getOrCreateTagBuilder(ItemTags.PIGLIN_FOOD).add(NItems.NEAR);
 
     }
 }
